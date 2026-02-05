02bcd985a91badc2e32c4a15d21902d0734bdea4
Ebuka <chukwuebukanwokike2007@gmail.com>
2026-02-05 23:18:47 +0100

    Fix?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -50,6 +50,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //             ->setStatusCode($response->getStatusCode());
         //     }
 
-            // return $response;
+            return $response;
         });
     })->create();
